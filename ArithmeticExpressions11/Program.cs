52058a29f1fa94f8883b83dd166a8e4d3fc3e7e2
Tatyana <tatyana.volkova1984@mail.ru>
2022-11-17 03:04:35 +0500

    a taske 11

---
+++ ArithmeticExpressions11/Program.cs
@@ -0,0 +1,4 @@
+﻿// Составить программу вывода на экран «столбиком» четырех любых чисел
+Console.WriteLine(new Random().Next(0, 10));
+Console.WriteLine(new Random().Next(0, 100));
+Console.WriteLine(new Random().Next(0, 1000));
