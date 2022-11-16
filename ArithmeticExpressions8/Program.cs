765b54d89710765fe8af48613a3e673aaac25d0f
Tatyana <tatyana.volkova1984@mail.ru>
2022-11-17 02:54:46 +0500

    a task 8

---
+++ ArithmeticExpressions8/Program.cs
@@ -0,0 +1,7 @@
+﻿/* Составить программу вывода на экран в одну строку четырех
+любых чисел с одним пробелом между ними.*/
+int a = new Random().Next(0, 1000);
+int b = new Random().Next(0, 500);
+int c = new Random().Next(-100, 100);
+int d = new Random().Next(-1000, 0);
+Console.WriteLine($"{a} {b} {c} {d}");
