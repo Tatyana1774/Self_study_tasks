2d9e22f6b3d2462c8efbae19af3a7e869416f808
Tatyana <tatyana.volkova1984@mail.ru>
2022-11-17 02:48:52 +0500

    a taske 7

---
+++ ArithmeticExpressions7/Program.cs
@@ -0,0 +1,5 @@
+﻿//Составить программу вывода на экран в одну строку трех любых чисел с двумя пробелами между ними.
+int a4 = new Random().Next(0, 50);
+int b4 = new Random().Next(0, 10);
+int c4 = new Random().Next(-10, 10);
+Console.WriteLine($"{a4}  {b4}  {c4}");
