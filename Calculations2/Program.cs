a71128f653cf7590df4d1a6095f223c89d11d5ff
Tatyana <tatyana.volkova1984@mail.ru>
2022-11-17 20:35:43 +0500

    Calculations by known formulas3

---
+++ Calculations2/Program.cs
@@ -0,0 +1,12 @@
+﻿//Дан радиус окружности. Найти ее диаметр. D = 2 × R
+Console.WriteLine("Введите радиус окружности");
+float r = Convert.ToSingle(Console.ReadLine());
+if(r > 0)
+{
+    float  D = 2 * r;
+    Console.WriteLine($"Диаметр окружности = {D} .\n", r);
+return;
+}
+else
+Console.Write("Введено не верное значение.");
+Console.ReadKey();
