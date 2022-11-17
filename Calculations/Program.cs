7d8f9b98b7d79308766a8954b4cf5accad613cdd
Tatyana <tatyana.volkova1984@mail.ru>
2022-11-17 20:21:53 +0500

    Calculations by known formulas 2

---
+++ Calculations/Program.cs
@@ -0,0 +1,13 @@
+﻿//Дана сторона квадрата. Найти его периметр. 
+Console.WriteLine("Введите длину стороны квадрата ");
+float L = Convert.ToSingle(Console.ReadLine());
+if(L > 0)
+{
+    float p = L * 4;
+    Console.WriteLine("\n Периметр квадрата = {0} .\n", p);
+return;
+}
+else
+Console.Write("Введено не верное значение.");
+Console.ReadKey();
+
