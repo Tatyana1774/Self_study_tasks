25a3ccdbd970d3cb4e2534957bccd775626e7ef5
Tatyana <tatyana.volkova1984@mail.ru>
2022-11-17 23:15:45 +0500

    Calculations by known formulas6

---
+++ Calculations7/Program.cs
@@ -0,0 +1,11 @@
+﻿//Даны два целых числа. Найти: а) их среднее арифметическое; б) их среднее геометрическое.
+{
+Console.WriteLine("укажи два целых числа через ENTER   ");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int averageArifm = (a + b) / 2;
+double averageGeom = Math.Sqrt(a * b);
+Console.WriteLine("Среднее арифмитическое {0} и {1} = {2}, а геометрическое {3}", a, b, averageArifm, averageGeom);
+Console.WriteLine(new string('-', 50));
+
+}
