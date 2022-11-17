05184527781f344565c7250b710c72f76cfa6dce
Tatyana <tatyana.volkova1984@mail.ru>
2022-11-17 21:28:08 +0500

    Calculations by known formulas5

---
+++ Calculations6/Program.cs
@@ -0,0 +1,10 @@
+﻿//Дан радиус окружности. Найти длину окружности и площадь круга. площадь круга S=πR² 2 длина окружности P=2πR
+{
+Console.WriteLine("Укажи радиус окружности  ");
+double r = double.Parse(Console.ReadLine());
+const double P = 3.14;
+double square = Math.Pow(r, 2) * P;
+double lenghtSirkl = 2 * r * P;
+Console.WriteLine("Длина окружности равна {0} м., а площаь равна {1} м2", lenghtSirkl, square);
+Console.WriteLine(new string('-', 50));
+}
