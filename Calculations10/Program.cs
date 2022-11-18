84c8b9c1d35b9e88d86a7014eecba0ef07c5cc39
Tatyana <tatyana.volkova1984@mail.ru>
2022-11-18 14:43:45 +0500

    Calculations by known formulas10

---
+++ Calculations10/Program.cs
@@ -0,0 +1,22 @@
+﻿//Составить программу решения линейного уравнения ax+b=0 (a ≠ 0).
+double a, b;
+double x;
+while (true)
+{
+Console.Write("Введите значение a: ");
+a = double.Parse(Console.ReadLine());
+if (a > 0)
+break;
+else if(a == 0)
+Console.WriteLine("Неверное значение а, повторите попытку ввода! ");
+else if(a<0)
+break;
+}
+
+Console.Write("Введите значение b: ");
+b = double.Parse(Console.ReadLine());
+
+x = (double) ((b / a) * (-1));
+
+Console.WriteLine("х равен = {0:0.00}", x);
+Console.ReadLine();
