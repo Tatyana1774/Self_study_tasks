2d9b9b557f76a73014833eec52c6ee1b5d98b163
Tatyana <tatyana.volkova1984@mail.ru>
2022-11-18 14:32:22 +0500

    Calculations by known formulas8

---
+++ Calculations9/Program.cs
@@ -0,0 +1,10 @@
+﻿/* Известны количество жителей в государстве и площадь его территории. Определить плотность населения в этом государстве. 
+P = A / S: P - плотность населения,  A - количество жителей, S - площадь территории */
+{
+Console.WriteLine("Укажите количество жителей  ");
+double A = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Укажите площадь территории государства в м2 ");
+double S = Convert.ToDouble(Console.ReadLine());
+double P = A / S;
+Console.WriteLine($"Плотность населения = {P} на м2");
+}
