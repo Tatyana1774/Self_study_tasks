8b3b67ba7ae52918ca837532b4dfe914b7932b8c
Tatyana <tatyana.volkova1984@mail.ru>
2022-11-18 14:19:37 +0500

    Calculations by known formulas7

---
+++ Calculations8/Program.cs
@@ -0,0 +1,10 @@
+﻿//Известны объем и масса тела. Определить плотность материала этого тела. р = m/V; m — масса вещества [кг] V — объем вещества [м 3]
+
+{
+Console.WriteLine("Укажите массу вещества в кг  ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Укажите объём вещества в м3 ");
+int v = Convert.ToInt32(Console.ReadLine());
+int p = m / v;
+Console.WriteLine($"Плотность материала этого тела = {p} кг/м3");
+}
