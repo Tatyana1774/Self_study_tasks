10b4e3e866c09cb2c9a957e2c22914f2e0b05877
Tatyana <tatyana.volkova1984@mail.ru>
2022-11-24 00:12:04 +0500

    the task of finding the hypotenuse is completed

---
+++ Calculations11/Program.cs
@@ -0,0 +1,12 @@
+﻿//Даны катеты прямоугольного треугольника. Найти его гипотенузу. c = √ (a 2+ b 2), где a - катет; b - катет; c - гипотенуза.
+double a = ReadInt("Введите значение первого катета a ");
+double b = ReadInt("Введите значение первого катета b ");
+double c = Math.Sqrt((a * 2) + (b * 2));
+Console.WriteLine($"Гипотенуза треугольника = {c}");
+
+
+double ReadInt(string text)
+{
+    Console.Write(text);
+    return Convert.ToDouble(Console.ReadLine());
+}
