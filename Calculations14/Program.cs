5c281d07cf9f25bcc5de35ea9a9b6347db0adc98
Tatyana <tatyana.volkova1984@mail.ru>
2022-11-24 00:44:26 +0500

    the task is to find the arithmetic mean and the geometric mean

---
+++ Calculations14/Program.cs
@@ -0,0 +1,11 @@
+﻿//Даны два числа. Найти среднее арифметическое и среднее геометрическое их  модулей.
+double ReadInt(string text)
+{
+    Console.Write(text);
+    return Convert.ToDouble(Console.ReadLine());
+}
+double a = ReadInt("Введите значение a -> ");
+double b = ReadInt("Введите значение b -> ");
+double srArif = (a + b) / 2;
+double srGeom = Math.Sqrt(a * b);
+Console.WriteLine($"Среднее арифметическое = {srArif}, а среднее геометрическое = {srGeom}");
