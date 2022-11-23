0e1574f748cecf42f46418160dc53e073e90e201
Tatyana <tatyana.volkova1984@mail.ru>
2022-11-24 00:33:10 +0500

    Finding the perimeter of a right triangle

---
+++ Calculations13/Program.cs
@@ -0,0 +1,12 @@
+﻿// Даны катеты прямоугольного треугольника. Найти его периметр. P=a+b+c
+int ReadInt(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int a = ReadInt("Введите значение катета a -> ");
+int b = ReadInt("Введите значение катета b -> ");
+int c = ReadInt("Введите значение катета c -> ");
+int P = a + b + c;
+Console.WriteLine($"Периметр треугольника = {P}");
