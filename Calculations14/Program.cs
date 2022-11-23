2f36b400ca59c8fd416e221284367cb0cb6358e4
Tatyana <tatyana.volkova1984@mail.ru>
2022-11-24 00:52:26 +0500

    corrected

--- Calculations14/Program.cs
+++ Calculations14/Program.cs
@@ -6,6 +6,14 @@ double ReadInt(string text)
 }
 double a = ReadInt("Введите значение a -> ");
 double b = ReadInt("Введите значение b -> ");
+if(a < 0)
+{
+    a *= -1;
+}
+else if(b < 0)
+{
+    b*= -1;
+}
 double srArif = (a + b) / 2;
-double srGeom = Math.Sqrt(a * b);
+double srGeom = Math.Round(Math.Sqrt(a * b), 2);
 Console.WriteLine($"Среднее арифметическое = {srArif}, а среднее геометрическое = {srGeom}");
