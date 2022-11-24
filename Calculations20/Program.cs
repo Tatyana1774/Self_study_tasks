b69670611aff9529824da0d26105f7b61904c2e3
Tatyana <tatyana.volkova1984@mail.ru>
2022-11-24 18:54:06 +0500

    task to find the perimeter of a trapezoid

---
+++ Calculations20/Program.cs
@@ -0,0 +1,13 @@
+﻿// Даны основания и высота равнобедренной трапеции. Найти периметр трапеции. P=a+b+c+d
+int ReadInt(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int a = ReadInt("Введите значение стороны a -> ");
+int b = ReadInt("Введите значение стороны b -> ");
+int c = ReadInt("Введите значение стороны c -> ");
+int d = ReadInt("Введите значение стороны d -> ");
+int P = a + b + c + d;
+Console.WriteLine($"Периметр трапеции = {P}");
