ad071ee9a94ee14c4dfb5e7841fd18fdfa5528dd
Tatyana <tatyana.volkova1984@mail.ru>
2022-11-24 12:44:11 +0500

    find the perimeter of an isosceles trapezoid

---
+++ Calculations15/Program.cs
@@ -0,0 +1,12 @@
+﻿//Даны основания и высота равнобедренной трапеции. Найти ее периметр. P = a + b + c + d
+int ReadInt(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int a = ReadInt("Введите значение a -> ");
+int b = ReadInt("Введите значение b -> ");
+int c = ReadInt("Введите значение c -> ");
+int d = ReadInt("Введите значение d -> ");
+int P = a + b + c + d;
+Console.WriteLine($"Периметр равнобедренной трапеции = {P} ");
