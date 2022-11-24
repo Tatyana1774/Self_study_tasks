912a8c7f5f9cc4f85641a650334950fde61c9c38
Tatyana <tatyana.volkova1984@mail.ru>
2022-11-24 13:03:40 +0500

    find the perimeter and diagonal of a rectangle

---
+++ Calculations16/Program.cs
@@ -0,0 +1,12 @@
+﻿//Даны стороны прямоугольника. Найти его периметр и длину диагонали. P=2(a+b);  d = √ (a 2+ b 2), a, b — стороны прямоугольника; d — диаметр прямоугольника.
+double ReadInt(string text)
+{
+    Console.Write(text);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+double a = ReadInt("Введите значение стороны a -> ");
+double b = ReadInt("Введите значение стороны b -> ");
+double P = 2 * (a + b);
+double d = Math.Sqrt((a * 2) + (b * 2));
+Console.WriteLine($"Периметр прямоугольника = {P}, а диаметр = {d}");
