157ccf6edd4e8fcb9e0e9a72eae2b867b825c4c2
Tatyana <tatyana.volkova1984@mail.ru>
2022-11-24 13:13:38 +0500

    the task is to find the sum, difference, product and quotient of dividing the first number by the second

---
+++ Calculations17/Program.cs
@@ -0,0 +1,14 @@
+﻿//Даны два числа. Найти их сумму, разность, произведение, а также частное от деления первого числа на второе.
+double ReadInt(string text)
+{
+    Console.Write(text);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+double num1 = ReadInt("Введите первое число -> ");
+double num2 = ReadInt("Введите второе число -> ");
+double sum = num1 + num2;
+double diff = num1 - num2;
+double comp = num1 * num2;
+double del = num1 / num2;
+Console.WriteLine($"Итак, сумма = {sum}, разность = {diff}, произведение = {comp}, частное = {del}");
